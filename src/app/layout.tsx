cc582739a3c63f9fccd3274043f482fe254dfcb8
Noe Barneron <noe.barneron@gmail.com>
2026-02-14 23:58:26 +0100

    fix: stats +bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Script from "next/script";
 import "./globals.css";
 import ClientProviders from '@/components/ClientProviders';
 import { DiscreetAds } from '@/components/DiscreetAds';
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({
           <DiscreetAds />
           {children}
         </ClientProviders>
+        <Analytics />
       </body>
     </html>
   );
